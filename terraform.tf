a25003b737a0bbaa1eddcab367cc2ff79c5b3979
Aman Patel <amanpatelit>
2025-10-14 13:02:31 +0530

    commented the s3 statefile

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,23 @@
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "devterraforms3"
+  force_destroy = true
+
+  tags = {
+    Name        = "TerraformStateBucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "devterraform"
   billing_mode = "PAY_PER_REQUEST"
@@ -16,12 +36,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
 
 # Backend Configuration
-terraform {
-backend "s3" {
- bucket         = "letscodeanddeploy"
- key            = "aws/terraform/terraform.tfstate"
- region         = "ap-south-1"
- dynamodb_table = "devterraform"
- encrypt        = true
- }
-}
+
+#terraform {
+#backend "s3" {
+# bucket         = "devterraforms3"
+# key            = "aws/terraform/terraform.tfstate"
+# region         = "ap-south-1"
+# dynamodb_table = "devterraform"
+# encrypt        = true
+# }
+#}
